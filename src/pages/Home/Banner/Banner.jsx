0a9540a6ac9faef8512bd3da2d209a18d21ad1f5
leon-dream1~ <nahidulislam019045@gmail.com>
2024-05-11 01:37:54 +0600

    Banner added

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,6 +1,17 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import Slide from "./Slide";
+// import Slide from "./Slide";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import { RiDrinks2Line } from "react-icons/ri";
+import {
+  MdEventAvailable,
+  MdFreeBreakfast,
+  MdLocalOffer,
+} from "react-icons/md";
+import { FaWifi } from "react-icons/fa";
+import { LuParkingCircle } from "react-icons/lu";
+import { FaGift } from "react-icons/fa";
+import { CiDiscount1 } from "react-icons/ci";
+import { CgGym } from "react-icons/cg";
 
 import "swiper/css";
 import "swiper/css/pagination";
@@ -16,20 +27,84 @@ const Banner = () => {
       }}
       autoplay={{
         delay: 2500,
-        disableOnInteraction: false,
       }}
       loop={true}
       modules={[Navigation, Pagination, Autoplay]}
       slidesPerView={1}
     >
       <SwiperSlide>
-        <Slide />
+        <div className={`h-[800px] bg-bgImg1 bg-no-repeat bg-cover`}>
+          <div className="h-[800px] flex flex-col items-center justify-center bg-black opacity-70">
+            <h1 className="text-[80px] font-von text-white font-extrabold">
+              Welcome to <span className="text-[#FFAC41]">Hotello</span>
+            </h1>
+            <div className="flex gap-8 space-x-[80px] mt-[50px]">
+              <div className="text-white font-extrabold flex flex-col items-center space-y-4">
+                <RiDrinks2Line size={50} />
+                <h4 className="text-[30px] font-merriweather font-bold">
+                  Drinks
+                </h4>
+              </div>
+              <div className="text-white font-extrabold flex flex-col items-center space-y-4">
+                <MdEventAvailable size={50} />
+                <h4 className="text-[30px] font-merriweather font-bold">
+                 Special Events
+                </h4>
+              </div>
+              <div className="text-white font-extrabold flex flex-col items-center space-y-4">
+                <CgGym size={50} />
+                <h4 className="text-[30px] font-merriweather font-bold">Gym</h4>
+              </div>
+            </div>
+          </div>
+        </div>
       </SwiperSlide>
       <SwiperSlide>
-        <Slide />
+        <div className={`h-[800px] bg-bgImg1 bg-no-repeat bg-cover`}>
+          <div className="h-[800px] flex flex-col items-center justify-center bg-black opacity-70">
+            <h1 className="text-[80px] font-von text-white font-extrabold">
+              Golden <span className="text-[#FFAC41] font-von">Autumn </span>
+              Offer
+            </h1>
+            <div className="flex gap-8 space-x-[80px] mt-[50px]">
+              <div className="text-white font-extrabold flex flex-col items-center space-y-4">
+                <FaWifi size={50} />
+                <h4 className="text-[30px] font-merriweather ">Free Wifi</h4>
+              </div>
+              <div className="text-white font-extrabold flex flex-col items-center space-y-4">
+                <LuParkingCircle size={50} />
+                <h4 className="text-[30px] font-merriweather">Free Parking</h4>
+              </div>
+              <div className="text-white font-extrabold flex flex-col items-center space-y-4">
+                <MdFreeBreakfast size={50} />
+                <h4 className="text-[30px] font-merriweather">BreakFast</h4>
+              </div>
+            </div>
+          </div>
+        </div>
       </SwiperSlide>
       <SwiperSlide>
-        <Slide />
+        <div className={`h-[800px] bg-bgImg1 bg-no-repeat bg-cover`}>
+          <div className="h-[800px] flex flex-col items-center justify-center bg-black opacity-70">
+            <h1 className="text-[80px] font-von text-white font-extrabold">
+              Enjoy <span className="text-[#FFAC41] font-von">The </span> Experience
+            </h1>
+            <div className="flex gap-8 space-x-[80px] mt-[50px]">
+              <div className="text-white font-extrabold flex flex-col items-center space-y-4">
+                <CiDiscount1 size={50} />
+                <h4 className="text-[30px] font-merriweather">Discount</h4>
+              </div>
+              <div className="text-white font-extrabold flex flex-col items-center space-y-4">
+                <MdLocalOffer size={50} />
+                <h4 className="text-[30px] font-merriweather">Best offer</h4>
+              </div>
+              <div className="text-white font-extrabold flex flex-col items-center space-y-4">
+                <FaGift size={50} />
+                <h4 className="text-[30px] font-merriweather">Special Gift</h4>
+              </div>
+            </div>
+          </div>
+        </div>
       </SwiperSlide>
     </Swiper>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        von : "'Herr Von Muellerhoff', cursive",
-        merriweather : '"Merriweather", serif',
-        rubik : '"Rubik", sans-serif',
+        von: "'Herr Von Muellerhoff', cursive",
+        merriweather: '"Merriweather", serif',
+        rubik: '"Rubik", sans-serif',
         montserrat: '"Montserrat", sans-serif',
-        roboto: '"Roboto", sans-serif'
-      }
+        roboto: '"Roboto", sans-serif',
+      },
+      backgroundImage: {
+        bgImg1: "url('/banner1.jpg')",
+        bgImg2: "url('/banner2.jpg')",
+        bgImg3: "url('/banner3.jpg')",
+        bgImg4: "url('/banner4.jpg')",
+      },
     },
   },
-  plugins: [require('daisyui'),],
-}
-
+  plugins: [require("daisyui")],
+};
